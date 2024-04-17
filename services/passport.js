d5dbb0961cbf79689fafa602bf8b44e83dd52ae6
jakegillespie6 <64338143+jakegillespie6@users.noreply.github.com>
2024-04-17 15:16:53 -0700

    test prod keys

--- services/passport.js
+++ services/passport.js
@@ -2,7 +2,6 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
 const mongoose = require('mongoose');
-
 const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
